ac45b20f32ab2f5aff0d154357fc51b9a5b243cb
feizhen <sysulz19920512@163.com>
2016-07-02 02:21:46 +0800

    finish

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -7,12 +7,25 @@
         .run(runBlock);
 
     /** @ngInject */
-    function runBlock($rootScope, $timeout, $state)
+    function runBlock($rootScope, $timeout, $state, Authentication)
     {
         // Activate loading indicator
-        var stateChangeStartEvent = $rootScope.$on('$stateChangeStart', function ()
+        var stateChangeStartEvent = $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams)
         {
             $rootScope.loadingProgress = true;
+
+            // 每次跳转时都要判断是否有用户已登录
+            if (!Authentication.isExists()) {
+                //如果没有则查询cookie中是否有用户信息
+                Authentication.getCookieUser().then(function(user) {
+                    //如果没有则前往登录页面
+                    if (!user) {
+                        $state.go("app.login");
+                        event.preventDefault();
+                    }
+                });
+            }
+
         });
 
         // De-activate loading indicator
---
+++ src/app/index.service.js
@@ -0,0 +1,60 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fuse')
+    .factory('Authentication', AuthenticationService);
+
+  function AuthenticationService($resource, $q, $cookies) {
+    var authticatedUser = null;
+
+    return {
+
+      isExists: function() {
+        return authticatedUser !== null;
+      },
+
+      getUser: function() {
+        return authticatedUser;
+      },
+
+      getCookieUser: function() {
+        var deferred = $q.defer();
+
+        var cookieUser = $cookies.getObject("cookieUser");
+        if (cookieUser) {
+          authticatedUser = cookieUser;
+        }
+        deferred.resolve(authticatedUser);
+
+        return deferred.promise;
+
+      },
+
+      login: function(params) {
+        return $resource('app/data/auth/users.json').get().$promise.then(function(result) {
+
+          var users = result.data.users;
+
+          for (var i = users.length - 1; i >= 0; i--) {
+            if (users[i].username === params.username && users[i].password === params.password) {
+              authticatedUser = users[i];
+              $cookies.putObject("cookieUser", authticatedUser);
+              return Promise.resolve(authticatedUser);
+            }
+          }
+
+          return Promise.resolve(authticatedUser);
+        });
+
+      },
+
+      logout: function() {
+        authticatedUser = null;
+        $cookies.remove("cookieUser");
+      }
+
+    }
+  }
+
+})();
